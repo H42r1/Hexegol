0499fabdaeca399d80a1bc22bf6d463cbb1e86f3
Harry YETE <harryete06@gmail.com>
2026-01-10 01:45:59 +0100

    Update Burpsuite script to copy executable to new path

--- burp.sh
+++ burp.sh
@@ -22,5 +22,5 @@ echo "Starting Key loader.jar..."
 echo "Executing Burpsuite Professional..."
 echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:/opt/my-resources/git_tools/Burpsuite-Professional/loader.jar -noverify -jar /opt/my-resources/git_tools/Burpsuite-Professional/burpsuite_pro_v2025.jar &" > burpsuitepro
 chmod +x burpsuitepro
-cp burpsuitepro ../../bin/burpsuitepro
+cp burpsuitepro ~/.exegol/my-resources/bin/burpsuitepro
 (./burpsuitepro)
