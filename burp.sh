4643e389a3296390fa2a7a1fb8f3848dea427128
Harry YETE <harryete06@gmail.com>
2026-01-10 12:10:23 +0100

    Refactor burp.sh to streamline installation and execution
    Removed installation of openjdk-21-jre and key generator execution. Updated commands for executing Burpsuite Professional.

--- burp.sh
+++ burp.sh
@@ -3,7 +3,7 @@
 # Installing Dependencies
 echo "Installing Dependencies..."
 sudo apt update
-sudo apt install git wget openjdk-21-jre -y
+sudo apt install git wget -y
 
 # Cloning
 git clone https://github.com/xiv3r/Burpsuite-Professional.git 
@@ -14,13 +14,8 @@ echo "Downloading Burp Suite Professional Latest..."
 version=2025
 wget -O burpsuite_pro_v$version.jar https://github.com/xiv3r/Burpsuite-Professional/releases/download/burpsuite-pro/burpsuite_pro_v$version.jar
 
-# Execute Key Generator
-echo "Starting Key loader.jar..."
-(java -jar loader.jar) &
-
 # Execute Burpsuite Professional
 echo "Executing Burpsuite Professional..."
 echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:/opt/my-resources/git_tools/Burpsuite-Professional/loader.jar -noverify -jar /opt/my-resources/git_tools/Burpsuite-Professional/burpsuite_pro_v2025.jar &" > burpsuitepro
 chmod +x burpsuitepro
 cp burpsuitepro ~/.exegol/my-resources/bin/burpsuitepro
-(./burpsuitepro)
